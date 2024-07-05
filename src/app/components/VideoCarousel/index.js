5e35a7d752ce3cc1c63bd7b00c99a7244d10fb4c
versacegl <timehinsamuel11@gmail.com>
2024-07-06 01:33:54 +0300

    Update index.js

--- src/app/components/VideoCarousel/index.js
+++ src/app/components/VideoCarousel/index.js
@@ -27,12 +27,7 @@ const VideoCarousel = () => {
   const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
 
   useGSAP(() => {
-    // slider animation to move the video out of the screen and bring the next video in
-    gsap.to("#slider", {
-      transform: `translateX(${-100 * videoId}%)`,
-      duration: 2,
-      ease: "power2.inOut", // show visualizer https://gsap.com/docs/v3/Eases
-    });
+ 
 
     // video animation to play the video when it is in the view
     gsap.to("#video", {
@@ -50,72 +45,69 @@ const VideoCarousel = () => {
     });
   }, [isEnd, videoId]);
 
-  useEffect(() => {
+  // Controller Part
+ useEffect(
+  ()=>{
     let currentProgress = 0;
-    let span = videoSpanRef.current;
-
-    if (span[videoId]) {
-      // animation to move the indicator
-      let anim = gsap.to(span[videoId], {
+    let span = videoSpanRef.current
+    const div = videoDivRef.current;
+    if(span[videoId]){
+      // animate the progress of the video  
+      let anim = gsap.to(span[videoId],{
         onUpdate: () => {
-          // get the progress of the video
-          const progress = Math.ceil(anim.progress() * 100);
-
-          if (progress != currentProgress) {
+          const progress = Math.ceil(anim.progress() * 100 );
+         
+          if(progress != currentProgress){
             currentProgress = progress;
 
-            // set the width of the progress bar
-            gsap.to(videoDivRef.current[videoId], {
+            gsap.to(div[videoId],{
               width:
-                window.innerWidth < 760
-                  ? "10vw" // mobile
-                  : window.innerWidth < 1200
-                  ? "10vw" // tablet
-                  : "4vw", // laptop
-            });
-
-            // set the background color of the progress bar
-            gsap.to(span[videoId], {
-              width: `${currentProgress}%`,
-              backgroundColor: "white",
-            });
+              window.innerWidth < 760
+              ? "10vw"
+              : window.innerWidth < 1200
+              ? "10vw"
+              :"4vw"
+            })
+          gsap.to(span[videoId],{
+            width:`${currentProgress}%`,
+            background:"white"
+          })
+           
           }
         },
+        onComplete:()=>{
+          if(isPlaying){
+            gsap.to(div[videoId],
+            {
+              width: "12px"
+            }
+          )
+          gsap.to(span[videoId],{
+            background:"#afafaf"
+          })
+        }
+        }
 
-        // when the video is ended, replace the progress bar with the indicator and change the background color
-        onComplete: () => {
-          if (isPlaying) {
-            gsap.to(videoDivRef.current[videoId], {
-              width: "12px",
-            });
-            gsap.to(span[videoId], {
-              backgroundColor: "#afafaf",
-            });
-          }
-        },
-      });
+    
+      })
+     // update the progress bar
+     const animUpdate = () => {
+      anim.progress(
+        videoRef.current[videoId].currentTime /
+          hightlightsSlides[videoId].videoDuration
+      );
+    };
 
-      if (videoId == 0) {
-        anim.restart();
-      }
-
-      // update the progress bar
-      const animUpdate = () => {
-        anim.progress(
-          videoRef.current[videoId].currentTime /
-            hightlightsSlides[videoId].videoDuration
-        );
-      };
-
-      if (isPlaying) {
-        // ticker to update the progress bar
-        gsap.ticker.add(animUpdate);
-      } else {
-        // remove the ticker when the video is paused (progress bar is stopped)
-        gsap.ticker.remove(animUpdate);
-      }
+    if (isPlaying) {
+      // ticker to update the progress bar
+      gsap.ticker.add(animUpdate);
+    } else {
+      // remove the ticker when the video is paused (progress bar is stopped)
+      gsap.ticker.remove(animUpdate);
     }
-  }, [videoId, startPlay]);
+    }
+  },[videoId, startPlay]
+ )
 
   useEffect(() => {
   
@@ -187,11 +179,11 @@ const VideoCarousel = () => {
                   preload="auto"
                   muted
                   ref={(el) => (videoRef.current[i] = el)}
-                  onEnded={() =>
-                    i !== 3
-                      ? handleProcess("video-end", i)
-                      : handleProcess("video-last")
-                  }
+                  // onEnded={() =>
+                  //   i !== 3
+                  //     ? handleProcess("video-end", i)
+                  //     : handleProcess("video-last")
+                  // }
                   onPlay={() =>
                     setVideo((pre) => ({ ...pre, isPlaying: true }))
                   }
