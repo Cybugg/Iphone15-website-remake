71b8d0876c2c44ef7b79c3e196f3ed4cb82e3694
versacegl <timehinsamuel11@gmail.com>
2024-07-10 09:11:23 +0300

    Update ModelView.js

--- src/app/components/Model/ModelView.js
+++ src/app/components/Model/ModelView.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import { Lights } from '../../components/Light'
 
 function ModelView({ index,groupRef,gsapType,controlRef,setRotationState,item,size}){
 
-  console.log(`I am the component props ll; ${props.size}`)
   return (
-   <section>
-        ModelView
-   </section>
+  <View
+  index={index}
+  id={gsapType} 
+  className={`border-2 border-red-500 h-full ${index ===2 ? "right-[-100%]" :"" }`}
+  >
+
+{/* Ambient Light */}
+<ambientLight intensity={0.3} />
+<PerspectiveCamera makeDefault position={[0,0,4]} />
+<Lights />
+<Suspense fallback={<div>
+  Loading ...
+</div>}>
+
+</Suspense>
+  </View>
   )
 }
 
