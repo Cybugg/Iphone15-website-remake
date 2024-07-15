8d632feef9cba91f5197117cd813c6dc0323969a
versacegl <timehinsamuel11@gmail.com>
2024-07-16 02:15:28 +0300

    update

--- src/app/components/Loader/index.js
+++ src/app/components/Loader/index.js
@@ -6,7 +6,7 @@ function Loader() {
     <Html>
         <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
             <div className='w-[10vh] h-[10vh] rounded-full' >
-               Loading...
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="none" stroke-opacity="1" stroke="#FF156D" stroke-width=".5" cx="100" cy="100" r="0"><animate attributeName="r" calcMode="spline" dur="2" values="1;80" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-width" calcMode="spline" dur="2" values="0;25" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" calcMode="spline" dur="2" values="1;0" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate></circle></svg> 
             </div>
         </div>
         </Html>
